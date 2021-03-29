e2014e54fb6352b544e9d7f31f0a12c86f967a4b
Alexandre Porto Alegre <alexandre.poaf@gmail.com>
2021-03-29 12:48:42 -0300

    Novas páginas e assets de Telemedicina

---
+++ pages/medicina/avulsos.js
@@ -0,0 +1,350 @@
+const especialidade = [
+        {
+            "nome": "ALERGOLOGIA",
+            "desc": "O Alergologista é o médico que faz o diagnóstico e tratamento de doenças alérgicas.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "CARDIOLOGIA",
+            "desc": "O Cardiologista é o médico que faz diagnóstico e tratamento dos problemas do coração. Cuida de doenças como hipertensão, AVC e arritmia.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "CLINICO GERAL",
+            "desc": "O Clínico Geral é o médico que trata de pacientes adultos, acompanhando diferentes doenças.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "DERMATOLOGIA",
+            "desc": "Dermatologista é o médico que faz diagnóstico e tratamento das doenças da pele, unhas e cabelos.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "ENDOCRINOLOGIA",
+            "desc": "O Endocrinologista é o médico que acompanha as glândulas endócrinas, responsáveis pela secreção dos hormônios. Trata de doenças da tireóide e diabetes, entre outras.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "GASTROENTEROLOGIA",
+            "desc": "O Gastroenterologista é o médico que faz o diagnóstico e tratamento das doenças do aparelho digestivo, como gastrite, prisão de ventre, úlcera péptica e doenças intestinais.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "GERIATRA",
+            "desc": "O Gastroenterologista é o médico que faz o diagnóstico e tratamento das doenças do aparelho digestivo, como gastrite, prisão de ventre, úlcera péptica e doenças intestinais.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "GINECOLOGIA/OBSTETRÍCIA",
+            "desc": "O Ginecologista é o médico que trata do organismo da mulher e de seu aparelho genital, fazendo o acompanhamento contínuo da saúde feminina, desde a sua menstruação, gravidez até o envelhecimento.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "INFECTOLOGIA",
+            "desc": "O Infectologista é o médico que cuida de pacientes com doenças infecciosas e parasitárias, como HIV, hepatites virais e dengue.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "NEFROLOGIA",
+            "desc": "O Nefrologista é o médico que trata das doenças do aparelho urinário, especialmente do rim.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "NUTRICIONISTA",
+            "desc": "O Nutricionista é o profissional de saúde que trabalha com educação alimentar, orientação de dietas e estudo dos alimentos.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "OFTALMOLOGIA ",
+            "desc": "Médico que acompanha as doenças relacionadas com os olhos (catarata, glaucoma, entre outras) e com a visão (como miopia, astigmatismo e vista cansada).",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "ORTOPEDIA",
+            "desc": "O Otorrinolaringologista é o médico que faz diagnóstico e tratamento dos problemas de ouvido, nariz e garganta. Cuida de doenças como otite, sinusite e faringite.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "OTORRINOLARINGOLOGIA",
+            "desc": "O Otorrinolaringologista é o médico que faz diagnóstico e tratamento dos problemas de ouvido, nariz e garganta. Cuida de doenças como otite, sinusite e faringite.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "PEDIATRIA",
+            "desc": "O Pediatra é o médico que atua na assistência a crianças e adolescentes, fazendo a prevenção e o tratamento de doenças e acompanhando o desenvolvimento de seus pacientes.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "PNEUMOLOGIA PEDIATRICA",
+            "desc": "O Pneumologista é o médico que cuida das doenças pulmonares e respiratórias, como gripe, asma e pneumonia.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "PNEUMOLOGIA",
+            "desc": "O Pneumologista é o médico que cuida das doenças pulmonares e respiratórias, como gripe, asma e pneumonia.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "PSICOLOGIA",
+            "desc": "O Psicólogo é o profissional que acolhe, avalia e auxilia na prevenção e tratamento de doenças mentais, distúrbios emocionais e de personalidade. Além disso acompanha atitude, sentimentos e mecanismos psíquicos do comportamento humano, por intermédio da análise de emoções, ideias e valores, atuando de forma terapêutica.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        },
+        {
+            "nome": "PSIQUIATRIA",
+            "desc": "O Psiquiatra é o médico que trata da prevenção, diagnóstico e tratamento dos sofrimentos mentais de cunho orgânico ou funcional, como depressão, transtorno bipolar e de ansiedade.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "REUMATOLOGIA",
+            "desc": "O Reumatologista é o médico que trata as doenças reumáticas, como artrite, esclerose, osteoporose e fibromialgia.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "UROLOGIA",
+            "desc": "O médico urologista trata do sistema urinário (rins, ureter e bexiga) de homens e mulheres e do sistema reprodutor/genital dos homens.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                },
+                {
+                    "parceiro": "docpass",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-docpass-tele.png"
+                }
+            ]
+        },
+        {
+            "nome": "TELEORIENTAÇÃO COVID-19",
+            "desc": "Médicos clínico geral, pneumologistas e infectoligistas com preparo para atendimento,  com objetivo de diminuir o risco de contato com outras pessoas, minimizando as demandas de emergências e outros serviços de saúde em sua região.",
+            "data": [
+                {
+                    "parceiro": "dr-consulta",
+                    "preco": "R$ 269,00 ",
+                    "descPreco": "ou 10x de R$ 26,90 sem juros",
+                    "logo": "logo-dr-consulta-footer.svg"
+                }
+            ]
+        }
+    ]
